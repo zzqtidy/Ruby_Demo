44ef867686d8746f527581df419f78bab2b23efa
tylerz <263720544@qq.com>
2016-11-01 11:49:54 +0800

    000

--- charpter02/Test02.rb
+++ charpter02/Test02.rb
@@ -16,13 +16,14 @@ end
 #Hash循环
 outcome={:参加费用=>1000,:挂件费用=>2000,:材料费用=>890}
 outcome.each{|key,value| puts "key=#{key},value=#{value}"}
+
 #hash还可以用一个参数（类似于数组的形式）来循环访问hash中的key和value
 outcome.each{|p| puts "#{p[0]}=#{p[1]}"}
+
 #File文件读取数据，each_line读取一行，另外还有each_char读取一个字符,each_byte读取一个字节。而其他对象中也有类似的each_xx的方法。
 file=File.open("../readme.txt")
 file.each_line{|line| puts "第一种形式读取一行数据,此方式操作完毕要关闭文件：#{line}"}
 file.close
-
 
 file1=File.open("../readme.txt")
 file1.each_line do |line|
@@ -30,9 +31,19 @@ file1.each_line do |line|
 end
 file1.close
 
-# File.open("../readme.txt") do |file2|
-# 	file2.each_line do |line|
-# 		puts "第三种形式读取文件中一行,此方式操作完毕不需要关闭文件：#{line}"
-# 	end
-# end
+File.open("../readme.txt") do |file2|
+	file2.each_line do |line|
+		puts "第三种形式读取文件中一行,此方式操作完毕不需要关闭文件：#{line}"
+	end
+end
+
+file3=File.open("../readme.txt")
+begin
+	file3.each_line do |line|
+		puts "第四种形式读取文件中一行,此方式操作完毕不需要关闭文件：#{line}"
+	end
+rescue Exception => e
+	puts "读取文件异常:"+e.message
+	file3.close
+end
 
